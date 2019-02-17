5886f82c5969ba0c65b5ecb192cc3cac35f529f6
Ankur Anant <ankuranant99@gmail.com>
2019-02-18 03:36:03 +0530

    multi bot support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const exp = require('express');
 const requestN = require('request');
 let fs = require('fs');
 let config = JSON.parse(fs.readFileSync("config.json"));
+let bots = JSON.parse(fs.readFileSync("botInfo.json"));
 
 const port = process.env.PORT || 3001;
 
@@ -10,25 +11,39 @@ const webhookURL = config.webhookURL;
 const app = exp();
 
 const web = config.web;
-let cnt = "yolo";
-let botName="Rusty"
-let status = ['UP', 'DOWN'];
-let statusDesc = ['Back Online', 'no longer responding'];
-let isDown = false;
-let notified = false;
+let content = "**ALERT**";
+// let status = ['UP', 'DOWN'];
+// let statusDesc = ['Back Online', 'no longer responding'];
 
-const notifier = (botName, status, statusDesc) => {
+let status = [
+	{
+		"ud": "UP",
+		"desc": "Back Online",
+		"color": "3201040"
+	},
+
+	{
+		"ud": "DOWN",
+		"desc": "no longer responding",
+		"color": "14177041"
+	}
+]
+
+let downArray = [];
+let notifiedArr = [];
+
+const notifier = (botName, sIndex) => {
 	requestN.post({
 		headers: {'content-type': 'application/json'},
 		url: webhookURL,
 		body: {
-				"username": "Status bot",
-				"content": cnt,
+				"username": "UpTimer",
+				"content": content,
 				"embeds": [
 					{
-						"title": `${botName} is ${status}`,
-						"color": "14177041",
-						"description": `Uptimer has detected **${botName} is ${statusDesc}**. Details can be found [here](https://google.com)`
+						"title": `${botName} is ${status[sIndex].ud}`,
+						"color": status[sIndex].color,
+						"description": `Uptimer has detected **${botName} is ${status[sIndex].desc}**. Details can be found [here](https://google.com)`
 					}
 				]
 			},
@@ -39,27 +54,38 @@ const notifier = (botName, status, statusDesc) => {
 setInterval(
 
 	function(){
-		requestN.get( web, (err, res, body) => {
 
-			if(res != undefined) {
-				if(isDown) {
-					isDown = false;
-					notifier(botName, status[0], statusDesc[0]);
-				}
-				console.log('status : ' + res.statusCode);
-			} else {
-				if(!notified) {
-					notified = true;
-					isDown = true;
-					notifier(botName, status[1], statusDesc[1]);
-				}
-				console.log('status : OFFLINE');
-			}
+		for(let i=0; i<bots.length; i++) {
 
-			if(err){
-				console.log(err);
-			}
-		});
+			requestN.get( bots[i].ip, (err, res, body) => {
+
+				if(res != undefined) {
+					if(downArray.includes(bots[i].name)) {
+
+						downArray.splice(downArray.indexOf(bots[i].name), 1);
+						notifiedArr.splice(notifiedArr.indexOf(bots[i].name), 1);
+
+						notifier(bots[i].name, 0);
+					}
+					console.log(`${bots[i].name}'s status :  ${res.statusCode}`);
+				} else {
+					if(!notifiedArr.includes(bots[i].name)) {
+
+						notifiedArr.push(bots[i].name);
+						downArray.push(bots[i].name)
+						notifier(bots[i].name, 1);
+
+					}
+					console.log(`${bots[i].name} : OFFLINE`);
+				}
+	
+				if(err){
+					console.log(err);
+				}
+			});
+
+		}
+		
 	}, 5000); // <-- ping frequency
 
 app.listen(port, () => {
